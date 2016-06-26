cd9f9a6d7b6c8c25dfd6f44fea4d44e9924ad4ae
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2016-06-27 00:46:45 +0700

    + method post, put delete API

--- routes/genders.js
+++ routes/genders.js
@@ -10,4 +10,22 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+router.get('/count', function(req, res, next){
+	sql = "select count(*) as count from gender";
+	connection.query(sql, function(err, results){
+		res.send(results);
+	});
+});
+
+router.post('/',function(req, res, next){
+	res.send("add");
+});
+
+router.put('/',function(req, res, next){
+	res.send("edit");
+});
+router.delete('/', function(req, res, next){
+	res.send("delete");
+});
+
 module.exports = router;
