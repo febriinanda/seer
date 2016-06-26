4e9aba34b26b044cab45a952241d0f3acfa8aacc
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2016-06-27 02:11:22 +0700

    fix create, update, delete and insert

--- routes/genders.js
+++ routes/genders.js
@@ -10,22 +10,49 @@ router.get('/', function(req, res, next) {
 	});
 });
 
-router.get('/count', function(req, res, next){
+router.get('/count/', function(req, res, next){
 	sql = "select count(*) as count from gender";
 	connection.query(sql, function(err, results){
 		res.send(results);
 	});
 });
 
-router.post('/',function(req, res, next){
-	res.send("add");
+router.get('/:id', function(req, res, next) {
+	sql = "select * from gender where id_gender="+req.params.id;
+	connection.query(sql,function(err, results){
+		getResult(res, err, results);
+	});
 });
 
-router.put('/',function(req, res, next){
-	res.send("edit");
+router.post('/',function(req, res, next){
+	param = req.body;
+	console.log(param.name);
+	sql = "insert into gender(name) values('"+param.name+"')";
+	connection.query(sql, function(err, results){
+		getResult(res, err, results);
+	});
 });
-router.delete('/', function(req, res, next){
-	res.send("delete");
+
+router.put('/:id',function(req, res, next){
+	var param = req.body;
+	sql = "update gender set name='"+param.name+"' where id_gender="+req.params.id;
+	connection.query(sql, function(err, results){
+		getResult(res, err, results);
+	});
 });
+router.delete('/:id', function(req, res, next){
+	sql = "delete from gender where id_gender="+req.params.id;
+	connection.query(sql, function( err, results){
+		getResult(res, err, results);
+	});
+});
+
+function getResult(res, err, results){
+	if(err){
+		res.send(err);
+	}else{
+		res.send(results);
+	}
+}
 
 module.exports = router;
