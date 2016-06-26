a8f8db0d8ff140e75d6683e5dac27cd62423c49c
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2016-06-27 01:59:13 +0700

    fixed create, update, insert and delete

--- routes/religions.js
+++ routes/religions.js
@@ -6,25 +6,53 @@ var debug = require('debug')('seer:religion');
 router.get('/', function(req, res, next) {
 	sql = "select * from religion";
 	connection.query(sql,function(err, results){
-		res.send(results);
+		getResult(res, err, results);
 	});
 });
 
 router.get('/count', function(req, res, next){
 	sql = "select count(*) as count from religion";
 	connection.query(sql, function(err, results){
-		res.send(results);
+		getResult(res, err, results);
+	});
+});
+
+router.get('/:id', function(req, res, next) {
+	sql = "select * from religion where id_religion="+req.params.id;
+	connection.query(sql,function(err, results){
+		getResult(res, err, results);
 	});
 });
 
 router.post('/',function(req, res, next){
-	res.send("add");
+	param = req.body;
+	console.log(param.name);
+	sql = "insert into religion(name) values('"+param.name+"')";
+	connection.query(sql, function(err, results){
+		getResult(res, err, results);
+	});
 });
 
-router.put('/',function(req, res, next){
-	res.send("edit");
+router.put('/:id',function(req, res, next){
+	var param = req.body;
+	sql = "update religion set name='"+param.name+"' where id_religion="+req.params.id;
+	connection.query(sql, function(err, results){
+		getResult(res, err, results);
+	});
 });
-router.delete('/', function(req, res, next){
-	res.send("delete");
+
+router.delete('/:id', function(req, res, next){
+	sql = "delete from religion where id_religion="+req.params.id;
+	connection.query(sql, function( err, results){
+		getResult(res, err, results);
+	});
 });
+
+function getResult(res, err, results){
+	if(err){
+		res.send(err);
+	}else{
+		res.send(results);
+	}
+}
 module.exports = router;
